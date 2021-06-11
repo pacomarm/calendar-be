91a19c8c226e3325a4d9b60629958b6c124d490c
pacomarm <francisco.marmolejoml@udlap.mx>
2021-06-10 22:44:17 -0500

    user login and create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const {response} = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs')
 
 const createUser = async(req,res = response) => {
 
@@ -14,6 +15,10 @@ const createUser = async(req,res = response) => {
             })
         }
         user = new User(req.body);
+
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt)
+
         await user.save();
         res.status(201).json({
             ok: true,
@@ -29,16 +34,43 @@ const createUser = async(req,res = response) => {
     }
 }
 
-const loginUser = (req,res = response) => {
+const loginUser = async(req,res = response) => {
 
     const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email})
 
-    res.status(201).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+        if (!user){
+            return res.status(500).json({
+                ok: false,
+                msg: 'User DNE bro',
+            })
+        }
+
+        // confirm password
+        const validPassword = bcrypt.compareSync( password, user.password );
+
+        if(!validPassword){
+            return res.status(500).json({
+                ok: false,
+                msg: 'Wrong pwd bro',
+            })
+        }
+
+        // Generar JWT
+        return res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token: 'gg'
+        })
+    } catch (e){
+        console.log(e);
+        return res.status(500).json({
+            ok: false,
+            msg: 'DB error bro',
+        })
+    }
 }
 
 const revalidateToken = (req,res = response) => {
