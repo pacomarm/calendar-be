f864a61e16c657f527c61a320da1f26cbbe5f814
pacomarm <francisco.marmolejoml@udlap.mx>
2021-06-10 19:26:54 -0500

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
- console.log('hello');
+const express = require('express')
+require('dotenv').config()
+
+// create server
+const app = express();
+
+//Public dir
+app.use( express.static('public') )
+
+// routes
+// app.get('/', (req,res) => {
+//     console.log('requires slash bro');
+// })
+
+// listen to reqs
+app.listen( process.env.PORT, () => {
+    console.log('Server runnin on port', process.env.PORT)
+} )
