c941a6966ff5cb49861365f8704bb11e462de682
pacomarm <francisco.marmolejoml@udlap.mx>
2021-06-12 13:51:09 -0500

    parameters validation

--- controllers/events.js
+++ controllers/events.js
@@ -7,6 +7,9 @@ const getEvents = async(req,res = response) => {
     })
 }
 const createEvent = async(req,res = response) => {
+
+    console.log(req.body)
+
     res.json({
         ok: true,
         msg: 'create event'
--- routes/events.js
+++ routes/events.js
@@ -3,12 +3,28 @@ const { getEvents, createEvent, updateEvent, deleteEvent } = require('../control
 const { validateJWT } = require('../middlewares/validar-jwt')
 const router = Router()
 
+//to check parameters
+const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos')
+const { isDate } = require('../helpers/isDate')
+
 // apply JWT validation to all endpoints
 router.use( validateJWT );
 
 router.get('/', getEvents);
 
-router.post('/', createEvent);
+router.post(
+    '/',
+    [
+        check('title', 'Title is mandatory!').notEmpty(),
+        check('start', 'Start Date is mandatory!').notEmpty(),
+        check('start', 'Erroneous Start Date format!').custom(isDate),
+        check('end', 'End Date is mandatory!').notEmpty(),
+        check('end', 'Erroneous End Date format!').custom(isDate),
+        validarCampos
+    ],
+    createEvent
+);
 
 router.put('/:id', updateEvent);
 
